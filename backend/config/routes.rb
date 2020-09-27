ee3c5f9fbb272f51efcccea604ef3ef3759dac92
connorworthen <connorw220@protonmail.com>
2020-09-27 15:14:55 -0700

    initial data

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'users/index'
 
   resources :conversions
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
